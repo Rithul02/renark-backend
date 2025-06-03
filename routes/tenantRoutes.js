14b7b6108ae2238506c8faf815655a4b55c81e89
Rithul M <rithul0206@gmail.com>
2025-06-03 22:42:19 +0530

    Fix: router not defined in tenantRoutes.js

--- routes/tenantRoutes.js
+++ routes/tenantRoutes.js
@@ -1,3 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
 const { getTenantPayments } = require("../controllers/tenantController");
 
 router.get("/payments", getTenantPayments);
+
+module.exports = router;
